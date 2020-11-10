b45d0d67d92c46bc59e190908df9216505d2a6c6
xingyu123321 <2657270246@qq.com>
2020-11-10 15:30:01 +0800

    add trend

--- app/controllers/bank_budgets_controller.rb
+++ app/controllers/bank_budgets_controller.rb
@@ -8,6 +8,197 @@ class BankBudgetsController < ApplicationController
     @bank_budgets = @q.result.order(:fiscal_year).page(params[:page]).per(10)
   end
 
+  def trend
+    @unit_arr2015 = []
+    @final_arr2015 = []
+    @budget2015 = []
+    @funds2015 = []
+
+    @unit_arr2016 = []
+    @final_arr2016 = []
+    @budget2016 = []
+    @funds2016 = []
+
+    @unit_arr2017 = []
+    @final_arr2017 = []
+    @budget2017 = []
+    @funds2017 = []
+
+    @unit_arr2018 = []
+    @final_arr2018 = []
+    @budget2018 = []
+    @funds2018 = []
+
+    @unit_arr2019 = []
+    @final_arr2019 = []
+    @budget2019 = []
+    @funds2019 = []
+
+    @unit_arr2020 = []
+    @final_arr2020 = []
+    @budget2020 = []
+    @funds2020 = []
+
+    @unit_arr2021 = []
+    @final_arr2021 = []
+    @budget2021 = []
+    @funds2021 = []
+
+    BankBudget.where(fiscal_year: "2015").each do |bank_budget|
+      @unit_arr2015 << bank_budget.unit
+      @budget2015 << bank_budget.bank_budget
+      @funds2015 << bank_budget.all_funds
+    end
+    @final_arr2015 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2015
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2015
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2016").each do |bank_budget|
+      @unit_arr2016 << bank_budget.unit
+      @budget2016 << bank_budget.bank_budget
+      @funds2016 << bank_budget.all_funds
+    end
+    @final_arr2016 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2016
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2016
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2017").each do |bank_budget|
+      @unit_arr2017 << bank_budget.unit
+      @budget2017 << bank_budget.bank_budget
+      @funds2017 << bank_budget.all_funds
+    end
+    @final_arr2017 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2017
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2017
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2018").each do |bank_budget|
+      @unit_arr2018 << bank_budget.unit
+      @budget2018 << bank_budget.bank_budget
+      @funds2018 << bank_budget.all_funds
+    end
+    @final_arr2018 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2018
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2018
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2019").each do |bank_budget|
+      @unit_arr2019 << bank_budget.unit
+      @budget2019 << bank_budget.bank_budget
+      @funds2019 << bank_budget.all_funds
+    end
+    @final_arr2019 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2019
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2019
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2020").each do |bank_budget|
+      @unit_arr2020 << bank_budget.unit
+      @budget2020 << bank_budget.bank_budget
+      @funds2020 << bank_budget.all_funds
+    end
+    @final_arr2020 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2020
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2020
+      }
+    ].to_json
+
+    BankBudget.where(fiscal_year: "2021").each do |bank_budget|
+      @unit_arr2021 << bank_budget.unit
+      @budget2021 << bank_budget.bank_budget
+      @funds2021 << bank_budget.all_funds
+    end
+    @final_arr2021 = [
+      {
+        name: 'Bank Budget (BB) (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @budget2021
+      },
+      {
+        name: 'All Funds (US$, Millions)',
+        type: 'line',
+        stack: '总量',
+        areaStyle: {},
+        data: @funds2021
+      }
+    ].to_json
+  end
+
   # GET /bank_budgets/1
   # GET /bank_budgets/1.json
   def show
